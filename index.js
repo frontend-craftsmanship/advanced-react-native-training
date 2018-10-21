8900f693f60d81f95bdc1f0dabfafada4ca1399f
Vitto Lewerissa <rvlewerissa@gmail.com>
2018-10-21 21:54:54 +0700

    Add Login Screen

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /** @format */
 
 import {AppRegistry} from 'react-native';
-import App from './src/screens/AddTransaction/AddTransaction';
+import App from './src/screens/Login/Login';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);

7c1a84b994086e8c84a6f10c953446864aeb3b5a
Vitto Lewerissa <rvlewerissa@gmail.com>
2018-11-11 23:15:15 +0700

    Add counter screen

---
+++ src/screens/Counter/Counter.js
@@ -0,0 +1,48 @@
+// @flow
+
+import React, {Component} from 'react';
+import {View, StyleSheet, TouchableOpacity} from 'react-native';
+
+import {Text} from '../../core-ui';
+
+export default class Counter extends Component<*, *> {
+  state = {
+    counter: 0,
+  };
+
+  render() {
+    return (
+      <View style={styles.root}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            this.setState({counter: this.state.counter + 1});
+          }}
+        />
+        <Text style={styles.counterText}>{this.state.counter}</Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            this.setState({counter: this.state.counter - 1});
+          }}
+        />
+      </View>
+    );
+  }
+}
+
+let styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    backgroundColor: '#ffc425',
+    alignItems: 'center',
+  },
+  button: {
+    flex: 1,
+    width: '100%',
+  },
+  counterText: {
+    fontSize: 130,
+    color: 'white',
+  },
+});
